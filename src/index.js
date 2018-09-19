fb71e39acd81e51366342d33cea940fde51222c0
Dusan Perkovic <dusan.perkovic@namics.com>
2018-09-19 21:19:20 +0200

    Array example done.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import Vue from 'vue/dist/vue.esm.js';
 new Vue({
     el: '#app',
     data: () => ({
-        test: 'test'
-    })
+        items: [
+            {
+                id: 1,
+                text: 'first one'
+            },
+            {
+                id: 2,
+                text: 'second one',
+            },
+            {
+                id: 3,
+                text: 'third one' 
+            }
+        ]
+    }),
+    methods: {
+        addToArray: function(newItemText) {
+            const lastItem = this.items.length - 1;
+            const newItemId = this.items[lastItem].id + 1;
+            this.items.push({
+                id: newItemId,
+                text: newItemText
+            });
+        }
+    }
 });
