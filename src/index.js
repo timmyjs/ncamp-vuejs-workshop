86e3b8235e88acd873dfc963f8bf5cac835b07a6
Dusan Perkovic <dusan.perkovic@namics.com>
2018-09-19 21:03:42 +0200

    Complete solution for the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
-import Vue from 'vue/dist/vue.esm.js';
+import Vue from 'vue';
+
+import {
+    getTopics,
+    createTopic,
+    updateTopic,
+    deleteTopic
+} from './dataMethods';
 
 new Vue({
     el: '#app',
     data: () => ({
-        test: 'test'
-    })
+        userName: '',
+        loggedIn: false,
+        newTopicName: '',
+        topics: []
+    }),
+    methods: {
+        logIn: function() {
+            this.loggedIn = true;
+
+            this.refreshData();
+        },
+
+        refreshData() {
+            getTopics()
+                .then(data => this.topics = data);
+        },
+
+        addTopic: function(topicName) {
+            createTopic(topicName)
+                .then(this.refreshData);
+        },
+
+        updateTopic: function({id, title}) {
+            updateTopic(id, title)
+                .then(this.refreshData);
+        },
+
+        removeTopic: function(topicId) {
+            deleteTopic(topicId)
+                .then(this.refreshData);
+        }
+    }
 });
